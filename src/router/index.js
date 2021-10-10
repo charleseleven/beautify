a3e87d64cb768e46d2fc1298ccfcd93452e9d680
Charles Pires Rodrigues <cpiresrodrigues81@gmail.com>
2021-10-10 14:20:07 -0300

    Create screen to Dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,20 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ '../views/About')
-    }
+        import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () =>
+        import('../views/Login.vue')
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () =>
+        import('../views/Dashboard.vue')
+    },
+
   ]
 })
